b636fa330f27285afbdbeb5ff46af4c7f59b42df
Olabosinde Oladipo <olabosindeoladdipo@gmail.com>
2020-08-15 13:42:16 +0100

    cron implementation

--- scrapers.js
+++ scrapers.js
@@ -2,19 +2,17 @@ const { setUpDataDirs, setUpReadLine, getDatafromURL, extractTableHTMLtoObject, 
 const CURRENT_DIR = './archive/current';
 const DATA_DIR = './archive/data';
 const MUTATIONS_DIR = './archive/mutations';
+var cron = require('node-cron');
 
-(async () => {
-    await setUpDataDirs(CURRENT_DIR, DATA_DIR, MUTATIONS_DIR);
-    // let rl = await setUpReadLine();
-    // rl.on("close", function () { // callback for catching 'close' event of prompt
-    //     process.exit(0);
-    // });
-    let url = await requestForUrl();
-    let identifier = await requestForIdentifier();
-    let $ = await getDatafromURL(url);
-    let extractedObject = await extractTableHTMLtoObject($, identifier);
-    await saveToArchive(extractedObject, url, DATA_DIR);
-    generateMutation(extractedObject, url, CURRENT_DIR, MUTATIONS_DIR).then((h) => {
-    });
-    
-})();
+cron.schedule('00 14 * * *', () => {
+    (async () => {
+        await setUpDataDirs(CURRENT_DIR, DATA_DIR, MUTATIONS_DIR);
+        let url = await requestForUrl();
+        let identifier = await requestForIdentifier();
+        let $ = await getDatafromURL(url);
+        let extractedObject = await extractTableHTMLtoObject($, identifier);
+        await saveToArchive(extractedObject, url, DATA_DIR);
+        generateMutation(extractedObject, url, CURRENT_DIR, MUTATIONS_DIR).then((h) => {
+        });
+    })();
+});
